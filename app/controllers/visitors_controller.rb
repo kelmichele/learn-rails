4f0b5f16f8dfc0118d07122b36b705d180cc0815
Kelli Parris <kelli@hsdesignhouse.com>
2017-01-11 13:46:10 -0600

    bootstrap, pages

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,13 +1,6 @@
 class VisitorsController < ApplicationController
 	def new
-    @owner = Owner.new
-    render 'visitors/new'
+	    @owner = Owner.new
 	end
-
-		# def new
-	 #    Rails.logger.debug 'DEBUG: entering new method'
-	 #    @owner = Owner.new
-	 #    Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
-	 #  end
 
 end
